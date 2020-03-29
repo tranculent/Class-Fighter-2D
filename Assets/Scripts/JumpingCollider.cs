7aff71d8575ef2856088086e23fb94db9e106050
Yanko <yanicha93@gmail.com>
2020-03-29 14:00:25 +0300

    Initial .cs files

---
+++ Assets/Scripts/JumpingCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingCollider : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    private bool collding = false;
+
+    private void Update()
+    {
+        GetComponent<BoxCollider2D>().transform.position = new Vector2(transform.position.x, player.transform.position.y - (player.transform.localScale.y));
+        Debug.Log(collding);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.name.Equals("Player"))
+            collding = true;
+    }
+
+    public bool isColliding() { return collding; }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 20f;
+    [SerializeField] float crounchingSpeed = .36f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GroundCollider groundCollider;
+
+    private CircleCollider2D jumpingColliderPoint;
+    //[SerializeField] Animator animator;
+    
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    private bool isFacingRight;
+
+    private float flipX;
+
+    private bool canJump = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+        jumpingColliderPoint = GetComponent<CircleCollider2D>();
+        isFacingRight = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Flip(flipX);
+        if (groundCollider.getisCollidingWithJumperCollider())
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        flipX = deltaX;
+
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+            transform.position = new Vector2(newXPos, transform.position.y);
+        }
+        /*else if (Input.GetButton("Crouch") && !isCrouching)
+        {
+            // 1. Halve the collider
+            float currentColliderSizeY = GetComponent<BoxCollider2D>().size.y;
+            float currentColliderSizeX = GetComponent<BoxCollider2D>().size.x;
+            GetComponent<BoxCollider2D>().size = new Vector2(currentColliderSizeX, currentColliderSizeY / 2f);
+            Debug.Log(GetComponent<BoxCollider2D>().size);
+            // 2. Change the x velocity by taking into account the crounching speed variable
+
+            var deltaY = Input.GetAxis("Crouch") * Time.deltaTime * crounchingSpeed;
+            var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+            var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+            transform.position = new Vector2(newXPos, newYPos);
+
+            isCrouching = true;
+        }*/
+        else
+        {
+            var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+            transform.position = new Vector2(newXPos, transform.position.y);
+        }
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !isFacingRight || horizontal < 0 && isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
+
+    public bool getIsFacingRight() { return isFacingRight; }
+}
